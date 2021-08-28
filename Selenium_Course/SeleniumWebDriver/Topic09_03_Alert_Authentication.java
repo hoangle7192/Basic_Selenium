afe6cdbca23b706e0144518117cf158fe4fee948
Hoang Le <huyhoang7192@gmail.com>
2021-08-28 21:53:04 +0700

    commit TC_08_01_Authentication_Alert commit TC_08_02_Authentication_Alert commit TC_09_Authentication_Alert_AutoIT

--- Selenium_Course/SeleniumWebDriver/Topic09_03_Alert_Authentication.java
+++ Selenium_Course/SeleniumWebDriver/Topic09_03_Alert_Authentication.java
@@ -2,6 +2,7 @@ package SeleniumWebDriver;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -9,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 // [Online 22] - Topic 22 (Handle Alert/ Authentication Alert) - https://www.youtube.com/watch?v=MtUdEvHqyiw
@@ -19,6 +21,10 @@ public class Topic09_03_Alert_Authentication {
 	JavascriptExecutor executor;
 	Alert alert;
 	WebDriverWait explicitWait;
+
+	// Khai bao Path cho AUTO IT
+	String authenChromeAutoIT = projectPath + "\\AutoIT\\authen_chrome.exe";
+	String authenFirefoxAutoIT = projectPath + "\\AutoIT\\authen_firefox.exe";
 
 	@BeforeClass
 	public void beforeClass() {
@@ -120,7 +126,54 @@ public class Topic09_03_Alert_Authentication {
 		//Switch qua 1 alert
 		//alert = driver.switchTo().alert();
 	}
-	
+
+    @Test
+    public void TC_08_01_Authentication_Alert() {
+		String password = "admin";
+		String loginUser = "admin";
+        String url = "http://" + loginUser + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";
+        driver.get(url);
+        Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+    }
+
+	@Test
+	public void TC_08_02_Authentication_Alert() {
+		// Sử dung khi không get trược tiếp url
+
+		String password = "admin";
+		String loginUser = "admin";
+
+		driver.get("http://the-internet.herokuapp.com/");
+
+		String[] basicAuthlink = driver.findElement(By.xpath("//a[text()='Basic Auth']")).getAttribute("href").split("//"); // lay ra thuoc tinh href
+		String url = basicAuthlink[0] + "//" + loginUser + ":" + password + "@" + basicAuthlink[1];
+		//System.out.println(basicAuthlink);
+
+		driver.get(url); // truyen ham vao
+
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+
+	}
+
+
+    // tam thoi Pending
+    //@Test
+    public void TC_09_Authentication_Alert_AutoIT() throws IOException {
+		String loginUser = "admin";
+		String password = "admin";
+		String url = "http://the-internet.herokuapp.com/basic_auth";
+
+		if(driver.toString().contains("chrome")) {
+			Runtime.getRuntime().exec(new String[] {authenChromeAutoIT, loginUser, password});
+		} else {
+			Runtime.getRuntime().exec(new String[] {authenFirefoxAutoIT, loginUser, password});
+		}
+
+		driver.get(url); // bắt buộc phải để dưới if else
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+    }
+
+
 
 	public void sleepInSecond(long timeoutInSecond) {
 		try {
